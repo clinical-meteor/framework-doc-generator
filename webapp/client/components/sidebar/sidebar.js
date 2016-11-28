e721fb97fd5b05090b3573775810e503fa09ee13
Abigail Watson <awatson1978@gmail.com>
2016-11-27 20:31:39 -0600

    epic 2015 schemas

--- webapp/client/components/sidebar/sidebar.js
+++ webapp/client/components/sidebar/sidebar.js
@@ -1,23 +1,5 @@
 
 
-
-// Template.sidebar.onRendered = function() {
-//   this.find('#sidebarMenuContents a')._uihooks = {
-//     insertElement: function(node, next) {
-//       $(node)
-//         .hide()
-//         .insertBefore(next)
-//         .fadeIn();
-//     },
-//     removeElement: function(node) {
-//       $(node).fadeOut(function() {
-//         this.remove();
-//       });
-//     }
-//   };
-// };
-
-
 Template.sidebar.events({
   'click #themingLink': function (){
     Router.go('/theming');
@@ -66,30 +48,7 @@ Template.sidebar.events({
 });
 
 Template.sidebar.helpers({
-  getUsername: function () {
-    if (Meteor.user()) {
-      if (Meteor.user().emails[0]) {
-        return Meteor.user().emails[0].address;
-      } else {
-        return "---";
-      }
-    } else {
-      return "Sign In";
-    }
-  },
   getConnectionStatus: function () {
     return Meteor.status().status;
-  },
-  email: function() {
-    return Meteor.user().emails[0].address;
-  },
-  lists: function() {
-    return Lists.find({userId: Meteor.userId()});
-  },
-  activeListClass: function() {
-    var current = Router.current();
-    if (current.route.name === 'checklistPage' && current.params._id === this._id) {
-      return 'active';
-    }
   }
 });
